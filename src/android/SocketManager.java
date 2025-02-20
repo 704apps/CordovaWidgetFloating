925e0c052c13b33aa58d6fa8ca0609e01e4c70e1
Cristian Barbosa <an.cristianbarbosa.an@gmail.com>
2025-02-20 15:14:22 -0300

    criando arquivo do socket

---
+++ src/android/SocketManager.java
@@ -0,0 +1,67 @@
+package com.plugin.widgetfloat;
+
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+
+public class SocketManager {
+    private static SocketManager instance;
+    private Socket socket;
+    private static final String TAG = "SocketManager";
+
+    private static final String SOCKET_URL = "https://mobilidade-api-node-uby.prod.704apps.com.br/";
+
+    private SocketManager() {
+        try {
+            socket = IO.socket(SOCKET_URL);
+        } catch (URISyntaxException e) {
+            Log.e(TAG, "Erro ao criar o socket: " + e.getMessage());
+        }
+    }
+
+    public static synchronized SocketManager getInstance() {
+        if (instance == null) {
+            instance = new SocketManager();
+        }
+        return instance;
+    }
+
+    public void connect() {
+        if (socket != null && !socket.connected()) {
+            socket.connect();
+            Log.d(TAG, "Socket conectado");
+        }
+    }
+
+    public void disconnect() {
+        if (socket != null && socket.connected()) {
+            socket.disconnect();
+            Log.d(TAG, "Socket desconectado");
+        }
+    }
+
+    public void emit(String event, JSONObject data) {
+        if (socket != null && socket.connected()) {
+            socket.emit(event, data);
+            Log.d(TAG, "Evento emitido: " + event + " com dados: " + data.toString());
+        } else {
+            Log.e(TAG, "Socket não está conectado. Impossível emitir evento.");
+        }
+    }
+
+    public void on(String event, Emitter.Listener listener) {
+        if (socket != null) {
+            socket.on(event, listener);
+        }
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+}
